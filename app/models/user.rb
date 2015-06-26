9d5f8b4d05db1ae8f79194409db0c410b00e51f0
Tim Beiko <t.beiko23@gmail.com>
2015-06-26 12:32:55 -0400

    Work on profiles

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,7 +22,8 @@ class User <ActiveRecord::Base
       user.first_name = auth.info.first_name
       user.last_name = auth.info.last_name
       user.email = auth.info.email
-      user.image = auth.info.image
+      user.image = auth.info.image + "?type=large"
+      binding.pry
       user.token = auth.credentials.token
       user.expires_at = Time.at(auth.credentials.expires_at)
       user.save!
