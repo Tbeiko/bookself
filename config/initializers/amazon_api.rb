86094541d393cc1b67e2fe2cd7a00e2900d54f86
Tim Beiko <t.beiko23@gmail.com>
2015-07-09 17:22:57 -0400

    Change Amazon API version

--- config/initializers/amazon_api.rb
+++ config/initializers/amazon_api.rb
@@ -2,4 +2,5 @@ Amazon::Ecs.configure do |options|
   options[:AWS_access_key_id] = ENV["AWS_ACCESS_KEY_ID"]
   options[:AWS_secret_key] = ENV["AWS_SECRET_KEY"]
   options[:associate_tag] = ENV["associate_tag"]
+  options[:version] = "2013-08-01"
 end
