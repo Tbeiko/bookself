f9529040ddb65c491cbde0a68f30502c359b65c4
Tim Beiko <t.beiko23@gmail.com>
2015-07-10 10:08:38 -0400

    Change initializer for identity

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,7 +2,7 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET']
-  provider :identity, fields: [:first_name, :last_name, :email, :avatar], 
+  provider :identity, fields: [:first_name, :last_name, :email], 
             on_login: lambda { |env| SessionsController.action(:new).call(env) },
             on_registration: lambda { |env| IdentitiesController.action(:new).call(env) },
             on_failed_registration: lambda { |env| IdentitiesController.action(:new).call(env) }
