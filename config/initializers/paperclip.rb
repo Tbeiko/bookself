da8641f6539e1d1d0e5b99573c56574041f55e4f
Tim Beiko <t.beiko23@gmail.com>
2015-07-10 11:46:53 -0400

    Change S3 Path

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,2 +1,2 @@
-Paperclip::Attachment.default_options[:url] = 'http://bookself-avatars.s3.amazonaws.com'
+Paperclip::Attachment.default_options[:url] = 'http://s3-us-west-2.amazonaws.com'
 Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
