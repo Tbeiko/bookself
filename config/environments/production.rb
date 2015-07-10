b13d620e265b65217b3aa00ad66e58fe83b716ef
Tim Beiko <t.beiko23@gmail.com>
2015-07-10 11:39:31 -0400

    Add Coma

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,14 +77,15 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-    # Amazon S3 settings for Paperclip uploads
+  # Amazon S3 settings for Paperclip uploads
   config.paperclip_defaults = {
     :storage => :s3,
     :s3_protocol => 'http',
     :s3_credentials => {
-      :bucket => 'bookself-avatars'
+      :bucket => 'bookself-avatars',
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_KEY']
     }
   }
+
 end
